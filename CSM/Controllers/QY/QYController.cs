55cc662def7dc011dd8be58114b81f4e285807d1
Yang.Li <happynnan@gmail.com>
2016-06-03 10:32:59 +0800

    方法

--- CSM/Controllers/QY/QYController.cs
+++ CSM/Controllers/QY/QYController.cs
@@ -11,15 +11,29 @@ namespace CSM.Controllers.QY
         //
         // GET: /QY/
 
-        public ActionResult IndexTask()
+         public ActionResult IndexTask(string guid)
         {
-            ViewBag.Models = ShowList();
-            return View();
+            Models.DB mdb = new Models.DB();
+
+            //任务单号
+            var taskList = mdb.Search<Models.TaskInfo>(i => i.GUID == "123456");
+            
+            //var l = from a in taskList join b in mdb.Search<Models.productinfo>(p=>p.VendorID) where a.CustomerID 
+
+
+
+           // var result = from
+
+
+
+
+           // ViewBag.Model = mdb.Search<Models.taskinfo>(i => i.GUID == "123456");
+            return View("IndexTask");
         }
 
         private IEnumerable<Models.Customer> ShowList()
         {
-            Models.DB mdb = new Models.DB();
+            Models.DB mdb = new Models.DB(); 
 
             return mdb.all<Models.Customer>();
         }
