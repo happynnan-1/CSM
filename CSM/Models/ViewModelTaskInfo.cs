6001c5a8d53d74aeb9c7b48eec6563080f12cfa4
Yang.Li <happynnan@gmail.com>
2016-06-03 10:34:38 +0800

    是

---
+++ CSM/Models/ViewModelTaskInfo.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CSM.Models
+{
+    public class ViewModelTaskInfo
+    {
+
+        public ViewModelTaskInfo()
+        { }
+
+        #region Model
+        private string _guid;
+        private string _taskid;
+        private string _customerName;
+        private string _productName;
+        private string _channel;
+        private string _content;
+        private string _level;
+        private string _adress;
+        private string _contactname;
+        private string _phone;
+        private string _email;
+        private DateTime? _createtime;
+        private string _remark;
+        /// <summary>
+        /// 
+        /// </summary>
+        public string GUID
+        {
+            set { _guid = value; }
+            get { return _guid; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string TaskID
+        {
+            set { _taskid = value; }
+            get { return _taskid; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string CustomerName
+        {
+            set { _customerName = value; }
+            get { return _customerName; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string ProductName
+        {
+            set { _productName = value; }
+            get { return _productName; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Channel
+        {
+            set { _channel = value; }
+            get { return _channel; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Content
+        {
+            set { _content = value; }
+            get { return _content; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Level
+        {
+            set { _level = value; }
+            get { return _level; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Adress
+        {
+            set { _adress = value; }
+            get { return _adress; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string ContactName
+        {
+            set { _contactname = value; }
+            get { return _contactname; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Phone
+        {
+            set { _phone = value; }
+            get { return _phone; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Email
+        {
+            set { _email = value; }
+            get { return _email; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public DateTime? CreateTime
+        {
+            set { _createtime = value; }
+            get { return _createtime; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Remark
+        {
+            set { _remark = value; }
+            get { return _remark; }
+        }
+        #endregion Model
+    }
+}
